007d61bbb9cc65006f446e10aeb77d0344156965
wpzborowski <wpzborowski@gmail.com>
2024-02-24 20:36:15 +0100

    Final version of app

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'package:dice_roll/gradient_container.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          Colors.blue,
+          Colors.lightBlueAccent,
+        ),
+      ),
+    ),
+  );
+}
